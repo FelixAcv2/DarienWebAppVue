03772bed04898d1b494837945313e74dda2699a6
Felix Acv2 <acevedo.felixmiguel@gmail.com>
2020-11-27 10:50:59 -0400

    Initial

---
+++ src/components/Default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Ing. Felix Acevedo</h2>
+    <p>Aplicación de prueba</p>
+    <p>Contactos</p>
+    <ul>
+      <li><b>Nombre</b>: Felix Miguel</li>
+      <li><b>Apellido</b>: Acevedo Ventura</li>
+      <li><b>Telefono Casa</b>: 809-971-3286</li>
+      <li><b>Celular</b>: 829-448-0203</li>
+      <li><b>Email</b>: acevedo.felixmiguel@gmail.com</li>
+    </ul>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Default'
+}
+</script>
